cab847b55a41e342ae8375e4895b8a53c97cacbb
Hoi Mei Tong <142815940+Kelly0604@users.noreply.github.com>
2023-12-07 15:15:27 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,42 @@
-def subtraction(x,y):
-    result = 0
-    result = x - y
-    return result
+from dotenv import load_dotenv
+import os
+from flask import Flask, redirect, render_template, request, url_for
+import openai
+
+# Load environment variables
+load_dotenv()
+
+app = Flask(__name__)
+openai.api_key = os.getenv("API_TOKEN")
+
+@app.route("/")
+def index():
+    """Return the index page."""
+    return render_template("index.html")
+
+def get_daily_schedule(tasks):
+    """
+    Generate a daily schedule based on the list of tasks.
+    """
+    prompt = f"Create a daily schedule based on these tasks:\n{tasks}\n"
+
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=prompt,
+        max_tokens=150
+    )
+    return response.choices[0].text.strip()
+
+@app.route("/schedule", methods=["GET", "POST"])
+def schedule():
+    """
+    Handle the scheduling functionality.
+    """
+    if request.method == "POST":
+        tasks = request.form.get("tasks")
+        schedule = get_daily_schedule(tasks)
+        return render_template("schedule.html", schedule=schedule)
+    return render_template("schedule_form.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
